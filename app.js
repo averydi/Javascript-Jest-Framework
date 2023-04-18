2c7f6ab01dfd9f0d548c50120109344ee91246cb
David Avery <avery.di@gmail.com>
2023-04-18 17:00:02 +0000

    Currency Tests

---
+++ app.js
@@ -0,0 +1,35 @@
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+// we declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInEuro){
+    let valueInYen = (valueInEuro / 1.2) * 127.9;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInEuro){
+    let valueInPound = (valueInEuro / 127.9) * .8;
+    return valueInPound;
+}
+// this is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// just a console log for ourselves.
+console.log(sum(7,3))
+
+// export the function to be used on other files 
+// (similar to the keyword `export` when using webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
